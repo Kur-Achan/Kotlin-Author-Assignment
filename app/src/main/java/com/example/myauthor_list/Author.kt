20772cf7cf597577cfce9a54fd2994f947688bc1
Kur-Achan <achananyiethkur@gmail.com>
2024-06-25 16:34:08 +0300

    add files

---
+++ app/src/main/java/com/example/myauthor_list/Author.kt
@@ -0,0 +1,4 @@
+package com.example.myauthor_list
+
+class Author {
+}
---
+++ app/src/main/java/com/example/myauthor_list/Authors_posts_item.kt
@@ -0,0 +1,20 @@
+package com.example.myauthor_list
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Authors_posts_item : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_authors_posts_item)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.ivPost)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
